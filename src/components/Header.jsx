81fbc587645bf3f7d2fa97d046fe647e2e9ece9d
ayush-1s <ayushsharma2119@.com>
2024-04-25 21:09:39 +0530

    image path changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Header(){
     return (
     <header className="header">
-        <img src="./images/skull.jpeg" alt="logo" className="logo"/>
+        <img src="/skull.jpeg" alt="logo" className="logo"/>
         
     <h4 className="header-project">React course</h4>
     </header>
